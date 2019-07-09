3a5ab5aaa0257399c1bd281eefdf5dad66592c9e
佑丞 張 <yochang861025@gmail.com>
2019-07-09 11:31:48 +0800

    20190709

--- McProtocolDemo/Form1.cs
+++ McProtocolDemo/Form1.cs
@@ -26,7 +26,7 @@ namespace McProtocolDemo
             base.OnShown(e);
 
             connect();
-            read();
+      
           
         }
 
@@ -39,8 +39,8 @@ namespace McProtocolDemo
         }
         public void connect()
         {
-            string ip = "192.168.2.12";
-            int port = 3000;
+            string ip = "192.168.1.63";
+            int port = 4000;
 
             mcProtocol = new McProtocol(ip, port);
             mcProtocol.Connect();
@@ -51,14 +51,14 @@ namespace McProtocolDemo
                 Text = "McProtocol disconnect";
         }
 
-        private void read()
+        private void read(int address)
         {
 
             if (mcProtocol == null || !mcProtocol.IsConnected)
                 return;
 
             short[] readData = new short[1];
-            var rtn = mcProtocol.ExecuteRead("D", 1000, 10, ref readData);
+            var rtn = mcProtocol.ExecuteRead("D", address, 1, ref readData);
 
             if (rtn != 0)
                 Console.WriteLine("Error code: " + rtn);
@@ -67,21 +67,23 @@ namespace McProtocolDemo
                 Console.WriteLine("Read success.");
                 // Data format convert. All convert function in McCommand
                 string strData = McCommand.ShortArrayToASCIIString(false, readData[0]);
-                string intDataStr = McCommand.ShortArrayToInt16String(readData[1]);
+                //string intDataStr = McCommand.ShortArrayToInt16String(readData[1]);
+                
             }
+          
         }
 
-        private void write()
+        private void write(int address ,string input_data)
         {
             if (mcProtocol == null || !mcProtocol.IsConnected)
                 return;
 
-            short[] writeData = McCommand.Int16StringToShortArray("1");
+            short[] writeData = McCommand.Int16StringToShortArray(input_data);
 
             // 1 word = 16 bit = 2 character
             //short[] writeData = McCommand.ASCIIStringToShortArray("HI");
 
-            int rtn = mcProtocol.ExecuteWrite("D", 1000, 1, writeData);
+            int rtn = mcProtocol.ExecuteWrite("D", address, 1, writeData);
 
             if (rtn != 0)
                 Console.WriteLine("Error code: " + rtn);
@@ -93,7 +95,7 @@ namespace McProtocolDemo
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            timer1.Start();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -111,35 +113,12 @@ namespace McProtocolDemo
 
         }
 
-        int tem = 1;
+        int i = 0;
+        int input_address = 0;
 
         private void button1_Click(object sender, EventArgs e)
         {
-            SV report = new SV();
-            
-            while (tem == 1)
-
-            {
-                report.getVolue();
-                label1.Text = report.temperatue.ToString("#0.00000");
-                label2.Text = report.pa.ToString("#0.00000");
-                label3.Text = report.rotationX.ToString();
-                label4.Text = report.rotationY.ToString();
-                label5.Text = report.rotationZ.ToString();
-
-                Thread.Sleep(1000);
-                label1.Update();
-                label2.Update();
-                label3.Update();
-                label4.Update();
-                label5.Update();
-
-                continue;
-                
-
-                //while迴圈修正，手動關閉
-            }
-
+            timer1.Start();
         }
 
         private void label1_Click_1(object sender, EventArgs e)
@@ -149,9 +128,159 @@ namespace McProtocolDemo
 
         private void button2_Click(object sender, EventArgs e)
         {
-            tem = 0;
+            timer1.Stop();
         }
 
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();//產生Form2的物件，才可以使用它所提供的Method
+
+            this.Visible = false;//將Form1隱藏。由於在Form1的程式碼內使用this，所以this為Form1的物件本身
+            f.Visible = true;//顯示form2
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) //目前clock為1000毫秒
+        {
+            SV report = new SV(); 
+
+            report.getVolue();//產生資料
+
+            label1.Text = report.temperatue.ToString();
+            label2.Text = report.pa.ToString();
+            label3.Text = report.rotationX.ToString();
+            label4.Text = report.rotationY.ToString();
+            label5.Text = report.rotationZ.ToString();//顯示最新一筆資料在表格中
+
+
+            label1.Update();
+            label2.Update();
+            label3.Update();
+            label4.Update();
+            label5.Update();
+
+
+            /* write(input_address, label1.Text);
+             input_address++;
+             write(input_address, label2.Text);
+             input_address++;
+             write(input_address, label3.Text);
+             input_address++;
+             write(input_address, label4.Text);
+             input_address++;
+             write(input_address, label5.Text);
+             input_address++; */
+
+            //read(input_address);
+
+            if (i <= 20)
+            {
+                chart1.Series[0].Points.AddXY(i, report.temperatue);
+                chart1.Series[1].Points.AddXY(i, report.pa);
+                chart1.Series[2].Points.AddXY(i, report.rotationX);
+                chart1.Series[3].Points.AddXY(i, report.rotationY);
+                chart1.Series[4].Points.AddXY(i, report.rotationZ);//更新圖ALL
+
+                chart2.Series[0].Points.AddXY(i, report.temperatue);//更新圖temperature
+                            
+                chart3.Series[0].Points.AddXY(i, report.pa);//更新圖PA
+
+                chart4.Series[0].Points.AddXY(i, report.rotationX);
+                chart4.Series[1].Points.AddXY(i, report.rotationY);
+                chart4.Series[2].Points.AddXY(i, report.rotationZ);//更新圖Rotations
+
+                chart2.ChartAreas["temperature"].AxisY.Maximum = 40;
+                chart2.ChartAreas["temperature"].AxisY.Minimum = 30;
+                chart3.ChartAreas["temperature"].AxisY.Maximum = 350;
+                chart3.ChartAreas["temperature"].AxisY.Minimum = 300;//限制y軸長度
+            }
+            else if (i >= 21) {
+                chart1.Series[0].Points.AddXY(i, report.temperatue);
+                chart1.Series[1].Points.AddXY(i, report.pa);
+                chart1.Series[2].Points.AddXY(i, report.rotationX);
+                chart1.Series[3].Points.AddXY(i, report.rotationY);
+                chart1.Series[4].Points.AddXY(i, report.rotationZ);//更新圖ALL
+
+                chart2.Series[0].Points.AddXY(i, report.temperatue);//更新圖temperature
+
+                chart3.Series[0].Points.AddXY(i, report.pa);//更新圖PA
+
+                chart4.Series[0].Points.AddXY(i, report.rotationX);
+                chart4.Series[1].Points.AddXY(i, report.rotationY);
+                chart4.Series[2].Points.AddXY(i, report.rotationZ);//更新圖Rotations
+
+                chart1.ChartAreas["temperature"].AxisX.Maximum = i;
+                chart1.ChartAreas["temperature"].AxisX.Minimum = i - 20;
+                chart2.ChartAreas["temperature"].AxisX.Maximum = i;
+                chart2.ChartAreas["temperature"].AxisX.Minimum = i - 20;
+                chart3.ChartAreas["temperature"].AxisX.Maximum = i;
+                chart3.ChartAreas["temperature"].AxisX.Minimum = i - 20;
+                chart4.ChartAreas["temperature"].AxisX.Maximum = i;
+                chart4.ChartAreas["temperature"].AxisX.Minimum = i - 20;//限制x軸長度
+
+                chart2.ChartAreas["temperature"].AxisY.Maximum = 40;
+                chart2.ChartAreas["temperature"].AxisY.Minimum = 30;
+                chart3.ChartAreas["temperature"].AxisY.Maximum = 350;
+                chart3.ChartAreas["temperature"].AxisY.Minimum = 300;//限制y軸長度
+
+            }
+
+            i++;
+
+
+
+            //Thread.Sleep(1000);
+            //continue;                
+
+            //while迴圈修正，手動關閉
+
+        }
+
+        private void chart2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) //temperature
+        {
+            chart1.Visible = false;
+            chart2.Visible = true;
+            chart3.Visible = false;
+            chart4.Visible = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e) //PA
+        {
+            chart1.Visible = false;
+            chart2.Visible = false;
+            chart3.Visible = true;
+            chart4.Visible = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e) //Rotations
+        {
+            chart1.Visible = false;
+            chart2.Visible = false;
+            chart3.Visible = false;
+            chart4.Visible = true;
+        }
+
+        private void button7_Click(object sender, EventArgs e) //ALL
+        {
+            chart1.Visible = true;
+            chart2.Visible = false;
+            chart3.Visible = false;
+            chart4.Visible = false;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
---
+++ McProtocolDemo/Form2.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace McProtocolDemo
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button1.Size = new System.Drawing.Size(109, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Monitor";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "ReportViewer";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(396, 246);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 400);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+    }
+}
